72b10aebe0903acb58826373d392885d6e2b9974
yingweiy <yingweiy@gmail.com>
2018-08-25 06:13:24 -0500

    changed neural network model for the visual banana problem, achieved 13 in 985 Episodes.

--- visual_navigation.py
+++ visual_navigation.py
@@ -2,9 +2,9 @@ from dqn import DQN
 import Environment
 import numpy as np
 
-env_name = 'basic_banana'
+env_name = 'visual_banana'
 
-env = Environment.CollectBanana(env_name, 'Banana.x86_64')
+env = Environment.CollectBanana(env_name, 'Banana.x86')
 dqn = DQN(env.name, env.state_size, env.action_size, env)
 env.train_mode = True
 scores = dqn.train(n_episodes=2000, target_score=13.0)
